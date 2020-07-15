7fc4a02deaa762eb52c3764b6cc02512979a0053
GuilhermeFujita <fujitaguilherme@gmail.com>
2020-07-14 23:20:34 -0300

    Application logout

--- src/hooks/AuthContext.tsx
+++ src/hooks/AuthContext.tsx
@@ -14,6 +14,7 @@ interface SignInCredentials{
 interface AuthContextData {
   user: object;
   signIn(credentials: SignInCredentials): Promise<void>;
+  signOut(): void;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
@@ -42,10 +43,17 @@ const AuthProvider: React.FC = ({ children }) => {
     localStorage.setItem('@GoBarber:user', JSON.stringify(user));
 
     setData({ token, user })
+  }, []);
+
+  const signOut = useCallback(async() => {
+    localStorage.removeItem('@GoBarber:token');
+    localStorage.removeItem('@GoBarber:user');
+
+    setData({} as AuthState);
   }, [])
 
   return (
-    <AuthContext.Provider value={{user: data.user, signIn}}>
+    <AuthContext.Provider value={{user: data.user, signIn, signOut}}>
       {children}
     </AuthContext.Provider>
   )
