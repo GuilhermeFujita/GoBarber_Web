302c79a799ef3ec13aae684065d8498d6acbea93
GuilhermeFujita <fujitaguilherme@gmail.com>
2021-03-11 00:24:38 -0300

    Upgrading dependencies and error fix

--- src/utils/getValidationErrors.ts
+++ src/utils/getValidationErrors.ts
@@ -1,15 +1,17 @@
- import { ValidationError } from 'yup';
+import { ValidationError } from 'yup';
 
-interface Errors{
+interface Errors {
   [key: string]: string;
 }
 
-export default function getValidationErrors(err: ValidationError): Errors{
-  const validationErrors:Errors = {};
+export default function getValidationErrors(err: ValidationError): Errors {
+  const validationErrors: Errors = {};
 
-  err.inner.forEach(error => {
-    validationErrors[error.path] = error.message;
-  })
+  err.inner.forEach((error) => {
+    if (error.path) {
+      validationErrors[error.path] = error.message;
+    }
+  });
 
-  return validationErrors
+  return validationErrors;
 }
